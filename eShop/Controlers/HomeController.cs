757f67265ff442c94b58f55c1dd5b28835de90f2
Roman Kiselyk <romankiselyk1913@gmail.com>
2019-12-15 00:48:03 +0200

    Add home page

---
+++ eShop/Controlers/HomeController.cs
@@ -0,0 +1,25 @@
+﻿using eShop.Data.Interfaces;
+using eShop.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.Controlers {
+    public class HomeController:Controller {
+        private readonly IAllPhones _phoneRepositiry;
+
+        public HomeController(IAllPhones phoneRepositiry) {
+            _phoneRepositiry = phoneRepositiry;
+        }
+
+        public ViewResult Index() {
+            var homePhones = new HomeViewModel {
+                FavouritePhones = _phoneRepositiry.getFavoritePhones
+            };
+
+            return View(homePhones);
+        }
+    }
+}
---
+++ eShop/ViewModels/HomeViewModel.cs
@@ -0,0 +1,11 @@
+﻿using eShop.Data.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace eShop.ViewModels {
+    public class HomeViewModel {
+        public IEnumerable<Phone> FavouritePhones { get; set; }
+    }
+}
